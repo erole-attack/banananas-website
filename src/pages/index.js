0be8e41931bae08d9bc6e8e17fb0bead83647b14
Jeroen Gunster <jeroen.gunster@gmx.com>
2020-04-28 23:59:46 +0200

    No more ERROR #95313
    replaced aphrodite depedency with gatsby-plugin-aphrodite

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 import About from "../components/about"
 import Services from "../components/services"
@@ -10,12 +11,14 @@ import Nav from "../components/nav"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div>
+  <Layout>
+    <SEO title="Page two" />
     <Header/>
     <About/>
     <Services/>
     <Concept/>
-  </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
 )
 
 export default IndexPage
