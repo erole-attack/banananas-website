25402c07ecd872b2c5f54adfc2f344729bcdcc76
Jeroen Gunster <jeroen.gunster@gmx.com>
2020-05-07 03:30:53 +0200

    Update services.js

--- src/components/services.js
+++ src/components/services.js
@@ -66,7 +66,7 @@ export default () => {
 const servicesStyles = StyleSheet.create({
 
   container: {
-    width: '72vw',
+    width: '78vw',
     height: '40vh',
     background: '#283237',
     borderRadius: '15px',
@@ -98,7 +98,7 @@ const servicesStyles = StyleSheet.create({
 
   grid: {
     display: 'flex',
-    flexWrap: 'wrap',
+    flexWrap: 'nowrap',
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
